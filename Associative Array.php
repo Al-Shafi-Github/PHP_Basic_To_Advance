200410f215395fefbaaa23f3ceff35a73ab82ead
Al-Shafi-Github <abdullah.shafi191@gmail.com>
2023-07-21 01:52:58 +0600

    Updating Associative Value operators

---
+++ Associative Array.php
@@ -0,0 +1,33 @@
+<?php
+ $captials= array("USA" => "Washington D.C",
+ "Japan" => "Koyto",
+ "South Korea" => "Seoul",
+ "India" => "New Delhi"
+ ) ;
+
+ //replacing elements of array
+echo $captials["USA"]= "Las Vegas". PHP_EOL;
+ 
+echo array_pop($captials).PHP_EOL;
+
+foreach($captials as $key => $value){
+    echo "{$key} = {$value}".PHP_EOL;
+}
+
+//only key values
+$keys = array_keys($captials);
+
+foreach($keys as $key){
+    echo "{$key}".PHP_EOL;
+}
+
+//Flipping the assoicative array
+
+$captials= array_flip($captials); 
+
+foreach($captials as $key => $value){
+    echo "{$key} = {$value}".PHP_EOL;
+}
+
+
+?>
