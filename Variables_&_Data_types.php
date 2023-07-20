b88a6ea64831194024319a61a381dec191ddb608
Al-Shafi-Github <abdullah.shafi191@gmail.com>
2023-07-20 22:31:35 +0600

    Updating Variables

--- Variables_&_Data_types.php
+++ Variables_&_Data_types.php
@@ -1,5 +1,14 @@
 <?php
-    echo hello;
-    echo bye;
-    echo damnnnnnnnn;
+   $age= 21;
+   $users = 2;
+   $quantity= 3;
+   $price=  3.5; 
+
+
+    echo "You are {$age} years old".PHP_EOL;
+    echo "Currently {$users} are online".PHP_EOL;
+    echo "i want ot buy {$quantity} apples".PHP_EOL;
+    echo "The mango is \${$price}".PHP_EOL;  //Special keyword to show the dollar sign
 ?>
+
+
