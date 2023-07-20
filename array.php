2d8349d68227fcde9e26646e9fd35d9bfcb978ef
Al-Shafi-Github <abdullah.shafi191@gmail.com>
2023-07-21 01:11:18 +0600

    Writing array operations

---
+++ array.php
@@ -0,0 +1,34 @@
+<?php
+
+$foods=  array("apple","Orange","Pineapple","Mango","Cherry");
+
+// $foods[0]= 'coconut';
+
+// echo $foods[0].PHP_EOL;
+array_push($foods, 'sugarcane','kiwi');
+
+foreach($foods as $food){
+    echo $food. PHP_EOL;
+   
+
+}
+
+$popped_array= array_pop($foods);
+echo $popped_array. PHP_EOL;
+
+echo '----------------'.PHP_EOL;
+
+foreach($foods as $food){
+    echo $food. PHP_EOL;    
+}
+echo '----------------'.PHP_EOL;
+
+$shifted_array= array_shift($foods);
+echo $shifted_array.PHP_EOL; //Shifted works like basically pop. 
+
+echo '----------------'.PHP_EOL;
+$reversed_foods = array_reverse($foods);
+
+foreach($reversed_foods as $food){
+    echo $food.PHP_EOL;
+}
