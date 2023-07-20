532ba5e1bb50fcd5ad8f9ea5fe42912e78ec4754
Al-Shafi-Github <abdullah.shafi191@gmail.com>
2023-07-20 22:46:01 +0600

    Writing condition using If-else statement

---
+++ If-Else Statements.php
@@ -0,0 +1,14 @@
+<?php
+    $age= 21;
+
+    If($age> 18){
+        echo 'You may enter the site';
+    }
+    elseif($age<18){
+        echo "You can't enter the site";
+    }
+    else{
+        echo 'Undefined Age';
+    }
+
+?>
