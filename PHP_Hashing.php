fbf644f3c935b2011c5968928fc2d261e00be448
Al-Shafi-Github <abdullah.shafi191@gmail.com>
2023-07-22 12:58:55 +0600

    password hashing and verify

--- PHP_Hashing.php
+++ PHP_Hashing.php
@@ -1,1 +1,10 @@
 <?php
+$password = 'rush123';
+
+$hash = password_hash($password, PASSWORD_DEFAULT);
+
+//Verify the password 
+if (password_verify('rush123', $hash)) {
+    echo 'Password is corerct';
+} else
+    echo 'Incorrect Password';
