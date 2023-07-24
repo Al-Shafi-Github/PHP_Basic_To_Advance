9d40b86506c6756c8674401027b41fb1021d1cd6
Al-Shafi-Github <abdullah.shafi191@gmail.com>
2023-07-24 11:02:53 +0600

    ading loop through the json

--- PHP-Json Object.php
+++ PHP-Json Object.php
@@ -1,18 +1,28 @@
 <?php
-$cars = array("Volvo", "BMW", "Toyota");
+$cars = array("Audi", "BMW", "Toyota");
 
 echo json_encode($cars);
 
 //associative array
-$age = array("Peter" => 35, "Ben" => 37, "Joe" => 43);
+$age = array("Shafi" => 35, "Rita" => 37, "Nida" => 43);
 
 echo json_encode($age) . PHP_EOL;
 
 
 //Decoding assoicative array
 
-$jsonobj = '{"Peter":3.5,"Ben":37,"Joe":"man"}';
+$jsonobj = '{"Shafi":3.5,"Rita":37,"Nida":"woman"}';
 
 print_r(json_decode($jsonobj, true));
 
 var_dump(json_decode($jsonobj, true));
+
+
+//Loop through the json
+$jsonobj = '{"Shafi":35,"Rita":37,"siam":43}';
+
+$obj = json_decode($jsonobj);
+
+foreach ($obj as $key => $value) {
+    echo $key . " => " . $value . "<br>";
+}
